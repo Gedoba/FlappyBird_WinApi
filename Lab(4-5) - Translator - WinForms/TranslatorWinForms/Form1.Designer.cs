63c03babca3cc107e4f45d20806d913e93cb3a45
Gedoba <32306344+Gedoba@users.noreply.github.com>
2018-03-24 18:00:00 +0100

    Layout finished

--- Lab(4-5) - Translator - WinForms/TranslatorWinForms/Form1.Designer.cs
+++ Lab(4-5) - Translator - WinForms/TranslatorWinForms/Form1.Designer.cs
@@ -34,8 +34,6 @@
             this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.Spanish = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.English = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
@@ -54,7 +52,8 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(805, 25);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,7 +63,7 @@
             this.loadToolStripMenuItem,
             this.exportToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
             this.fileToolStripMenuItem.Text = "File";
             this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
             // 
@@ -85,39 +84,35 @@
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(4, 30, 0, 4);
+            this.splitContainer1.Panel1MinSize = 200;
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
-            this.splitContainer1.Size = new System.Drawing.Size(484, 337);
-            this.splitContainer1.SplitterDistance = 161;
+            this.splitContainer1.Size = new System.Drawing.Size(805, 481);
+            this.splitContainer1.SplitterDistance = 212;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 2;
             // 
             // listView1
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Spanish,
-            this.English});
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Location = new System.Drawing.Point(4, 30);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.MinimumSize = new System.Drawing.Size(200, 400);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(161, 337);
+            this.listView1.Size = new System.Drawing.Size(208, 447);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // Spanish
-            // 
-            this.Spanish.Text = "Spanish";
-            // 
-            // English
-            // 
-            this.English.Text = "English";
             // 
             // tableLayoutPanel1
             // 
@@ -129,53 +124,60 @@
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 30);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(313, 334);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(579, 447);
+            this.tableLayoutPanel1.TabIndex = 3;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(307, 136);
+            this.richTextBox1.Size = new System.Drawing.Size(579, 204);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
             this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Location = new System.Drawing.Point(3, 195);
+            this.richTextBox2.Location = new System.Drawing.Point(0, 242);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(307, 136);
+            this.richTextBox2.Size = new System.Drawing.Size(579, 205);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.button1.Location = new System.Drawing.Point(116, 145);
-            this.button1.MaximumSize = new System.Drawing.Size(81, 37);
-            this.button1.MinimumSize = new System.Drawing.Size(81, 37);
+            this.button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(239, 208);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.MaximumSize = new System.Drawing.Size(100, 25);
+            this.button1.MinimumSize = new System.Drawing.Size(100, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 37);
+            this.button1.Size = new System.Drawing.Size(100, 25);
             this.button1.TabIndex = 2;
             this.button1.Text = "Translate";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 361);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(805, 481);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -199,8 +201,6 @@
         private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Spanish;
-        private System.Windows.Forms.ColumnHeader English;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
--- Lab(4-5) - Translator - WinForms/TranslatorWinForms/Form1.cs
+++ Lab(4-5) - Translator - WinForms/TranslatorWinForms/Form1.cs
@@ -23,57 +23,63 @@ namespace TranslatorWinForms
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            
-
             OpenFileDialog theDialog = new OpenFileDialog();
             theDialog.Title = "Open Text File";
             theDialog.Filter = "TXT files|*.txt";
-            theDialog.InitialDirectory = "C:\\Users\\286327\\source\\repos\\TranslatorWinForms\\TranslatorWinForms";
+            theDialog.InitialDirectory = "..//..//";
             if (theDialog.ShowDialog() == DialogResult.OK)
             {
-                //MessageBox.Show();
-            }
-
-            foreach (string line in File.ReadAllLines(theDialog.FileName.ToString()))
-            {
-                
-                foreach(char c in line)
+                foreach (string line in File.ReadAllLines(theDialog.FileName.ToString()))
                 {
-                    if (Char.IsLetter(c))
+
+                    foreach (char c in line)
                     {
-                        words.Add(line.Split(' '));
-                        break;
+                        if (Char.IsLetter(c))
+                        {
+                            words.Add(line.Split(' '));
+                            break;
+                        }
+                        else continue;
                     }
-                    else continue;
                 }
-                    
+                listView1.View = View.Details;
+                listView1.Columns.Add(words[0][0]);
+                listView1.Columns.Add(words[0][1]);
                 
-                
-            }
-            listView1.View = View.Details;
-            for (int i = 0; i<words.Count; i++)
-            {
-                listView1.Items.Add(
-                    new ListViewItem(new[]
-                    {
+
+                for (int i = 1; i < words.Count; i++)
+                {
+                    listView1.Items.Add(
+                        new ListViewItem(new[]
+                        {
                         words[i][0],
                         words[i][1]
-                    }));
+                        }));
+                }
+
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
             }
         }
-
         private void exportToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            for(int i=0; i<words.Count; i++)
-            {
-                File.AppendAllText("..//..//dictionaryES1.txt", words[i][0] + " " + words[i][1] + Environment.NewLine);
-            }
-            
-        }
+            SaveFileDialog openFileDialog1 = new SaveFileDialog();
+            SaveFileDialog theDialog = new SaveFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = "..//..//";
+            if (theDialog.ShowDialog() == DialogResult.OK)
 
+                for (int i = 0; i < words.Count; i++)
+            {
+                File.AppendAllText(theDialog.FileName.ToString(), words[i][0] + " " + words[i][1] + Environment.NewLine);
+            }
+
+        }
         private void fileToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
-    }
+    }  
 }
+
