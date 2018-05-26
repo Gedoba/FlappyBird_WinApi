20532d25cfba41bd4fd583b3956137a748e4ed1e
Gedoba <32306344+Gedoba@users.noreply.github.com>
2018-05-26 19:34:11 +0200

    WPF2 init

---
+++ Lab(10-11) - WarGame - WPF2/WarGame/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame
+{
+    class Card
+    {
+        int val;//Ace-King
+        int color;//4 of them
+        public Card()
+        {
+
+        }
+    }
+}
---
+++ Lab(10-11) - WarGame - WPF2/WarGame/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WarGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void deck1Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an Image element.
+            // Create an Image element.
+            //Image croppedImage = new Image();
+            //croppedImage.Width = 200;
+            //croppedImage.Margin = new Thickness(5);
+            BitmapSource bSource = new BitmapImage(new Uri("C:\\Users\\286327\\source\\repos\\WarGame\\WarGame\\cards.jpg"));
+            // Create a CroppedBitmap based off of a xaml defined resource.
+            CroppedBitmap cb = new CroppedBitmap(
+               bSource,
+               new Int32Rect(226, 0, 225, 315));       //select region rect
+            //croppedImage.Source = cb;
+            card1Img.Source = cb;
+            cb = new CroppedBitmap(bSource, new Int32Rect(226, 316, 225, 315));       //select region rect
+            card2Img.Source = cb;
+
+        }
+    }
+}
